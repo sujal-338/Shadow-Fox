fa748491984da1e70a2875d2800b0b1883539181
sujal shinde <sujalshinde47491@gmail.com>
2025-09-27 18:55:03 +0530

    Add files via upload
    variable code - 
    Create a variable called for and assign it a value 4. See what happens and find out the reason behind the behavior that you see.
    
    Reason -
    for is a reserved keyword in Python. Keywords are special words that Python uses for its own syntax (like if, while, class, def, etc.). Since for is used to create loops, Python does not allow you to use it as a variable name.

---
+++ variable_for.py
@@ -0,0 +1,5 @@
+for = 4
+print(for)
+# for is a reserved keyword in Python. 
+# Keywords are special words that Python uses for its own syntax (like if, while, class, def, etc.). 
+# Since for is used to create loops, Python does not allow you to use it as a variable name.
